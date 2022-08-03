2ed765616ff6c6c48f5b392094dff0f692be7230
Moin Khan <nurmoin05@gmail.com>
2022-08-03 22:31:02 +0600

    daisy ui customization completed]

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
+    themes: [
+      {
+        mytheme: {
+          "primary": "#176F6B",
+          "secondary": "#FFC000",
+          "accent": "#FFEDD5",
+          "neutral": "#F3F4F6",
+          "base-100": "#ffffff",
+          "info": "#98A8DD",
+          "success": "#1BBB70",
+          "warning": "#DF7E07",
+          "error": "#FA5C5C",
+        },
+      },
+    ],
   },
 }
