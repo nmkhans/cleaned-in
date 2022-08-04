0a334c49916a011c97d9c2dac62c7af495f5c578
Moin Khan <nurmoin05@gmail.com>
2022-08-04 23:21:27 +0600

    header menu"s links organized

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     const menuItems = () => {
         return (
             <>
-                <li><a href="">Link Item</a></li>
-                <li><a href="">Link Item</a></li>
-                <li><a href="">Link Item</a></li>
-                <li><a href="">Link Item</a></li>
-                <li><a href="">Link Item</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">Contact</a></li>
             </>
         )
     }
@@ -19,11 +19,18 @@ const Header = () => {
                 <input id="cleanedin-drawer" type="checkbox" className="drawer-toggle" />
 
                 <div className="drawer-content flex flex-col">
-                    <div className="w-full navbar bg-base-300">
-                        <div className="flex-1 px-2 mx-2">Cleaned It</div>
+                    <div className="w-full navbar bg-base-100">
+                        <div className="flex-1 px-2 mx-2 font-semibold text-xl">
+                            {/* <Link to="/">
+                                Cleaned It.
+                            </Link> */}
+                            <a >
+                                Cleaned It.
+                            </a>
+                        </div>
                         <div className="flex-none lg:hidden">
-                            <label for="cleanedin-drawer" className="btn btn-square btn-ghost">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                            <label htmlFor="cleanedin-drawer" className="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                             </label>
                         </div>
                         <div className="flex-none hidden lg:block">
@@ -38,7 +45,7 @@ const Header = () => {
                     {/* Page Content Content */}
                 </div>
                 <div className="drawer-side">
-                    <label for="cleanedin-drawer" className="drawer-overlay"></label>
+                    <label htmlFor="cleanedin-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
                         {
                             menuItems()
